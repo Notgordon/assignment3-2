69ae7543f562605ecdfd6f218d77e7d348f8fc5a
Notgordon <studentguac@gmail.com>
2015-01-30 15:48:17 -0800

    Done with Appointment and Now to Arrays

--- gist.js
+++ gist.js
@@ -1,24 +1,67 @@
 window.onload = function() {
+
+};
+
+var settings = null;
+
+/*Gist List Creation*/
+
+//Gist Object
+
+
+function createGistList(ul, lang){
+	
+	
+	
 	
 }
 
-function search(){
-	var request = new XMLHttpRequest();
-	var pageNum = document.getElementsByName('pageNum')[0];
-	var url = 'https://api.github.com/gists/public';
-	var params = {
-	
-	};
-	
-	url += '?' + urlStringify(params);
-}
-	
+
+/*AJAX Section*/
+
 
 function makeString(obj){
-	var str = []
+	var str = [];
 	for (var prop in obj){
-		var s = encodeURIComponent(prop) + '=' encodeURIComponent(obj[prop]);
-		str.push(s);
+		var g = encodeURIComponent(prop) + '=' + encodeURIComponent(obj[prop]);
+		str.push(g);
 	}
 	return str.join('&');
 }
+
+function search(){
+	
+	
+	//var pNum = document.getElementsByName('pageNum');
+
+	//for ( var i = document.getElementsByName('pageNum'); i > 0; i--)
+	//{
+	var request = new XMLHttpRequest(); // request created
+	if(!request){
+		throw 'Request Failed';
+	}
+	var url = 'https://api.github.com/gists'; //url initialized
+	var params = {
+	  page: document.getElementsByName('pageNum') //page number
+	};
+	
+	url += '?' + makeString(params); //new url created
+	
+	//Create JSON OBJECT
+	request.onreadystatechange = function(){
+		if(this.readyState == 4){
+			var gist = JSON.parse(this.responseText) // gist JSON object 
+			var language = null; //object properties
+			
+			//createGistList(document.getElementById('gistList'));
+		
+		}
+	}
+	
+	request.open('GET', url);
+	request.send();
+	//}
+	//window.location= url;
+}
+	
+
